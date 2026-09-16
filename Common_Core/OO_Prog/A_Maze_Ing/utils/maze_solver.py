74c5e014bbfb2217b99ee08f862e386cadfcb640
aspthiba <aspthiba@f2r7s22.paris.42.school>
2026-09-17 00:38:13 +0200

    maze_solver tentative

--- Common_Core/OO_Prog/A_Maze_Ing/utils/maze_solver.py
+++ Common_Core/OO_Prog/A_Maze_Ing/utils/maze_solver.py
@@ -4,14 +4,11 @@ import numpy as np
 class Node:
     def __init__(self):
         self.xy: tuple[int, int]
-        self.n: bool
-        self.e: bool
-        self.s: bool
-        self.w: bool
+        self.n: bool = False
+        self.e: bool = False
+        self.s: bool = False
+        self.w: bool = False
         self.dist: int = -1
-
-    def set_xy(self, x: int, y:int):
-        self.xy = tuple(x, y)
 
     def set_dir(self, node: int):
         if node & 8:
@@ -39,6 +36,55 @@ def maze_convert(maze: list[list[int]], nodemaze: list[list[Node]]) -> list[list
         x = 0
         y = y + 1
     return nodemaze
+
+
+def path_reconstruct(nodemaze: list[list[Node]], exit: tuple[int, int], entry: tuple[int, int]) -> str:
+    x = exit[1]
+    y = exit[0]
+    dists: list[int]
+    path: list[int]
+    result: str
+    dir: int = 0
+    while x != entry[1] and y != entry[0]:
+        if nodemaze[y][x].n == True:
+            dists.append(nodemaze[y - 1][x].dist)
+            dir = 1
+        if nodemaze[y][x].e == True:
+            if dists[0] > nodemaze[y][x + 1].dist:
+                dists.pop(0)
+                dists.append(nodemaze[y][x + 1].dist)
+                dir = 2
+        if nodemaze[y][x].s == True:
+            if dists[0] > nodemaze[y + 1][x].dist:
+                dists.pop(0)
+                dists.append(nodemaze[y + 1][x].dist)
+                dir = 3
+        if nodemaze[y][x].w == True:
+            if dists[0] > nodemaze[y][x - 1].dist:
+                dists.pop(0)
+                dists.append(nodemaze[y][x - 1].dist)
+                dir = 4
+        dists.pop(0)
+        path.insert(0, dir)
+        if dir == 1:
+            y = y - 1
+        if dir == 2:
+            x= x + 1
+        if dir == 3:
+            y = y + 1
+        if dir == 4:
+            y = x - 1
+    for node in path:
+        if node == 1:
+            result = result + "S"
+        if node == 2:
+            result = result + "W"
+        if node == 3:
+            result = result + "N"
+        if node == 4:
+            result = result + "E"
+    return result
+
 
 
 def maze_solver(entry: tuple[int, int], exit: tuple[int, int], maze: list[list[int]]) -> str:
@@ -84,3 +130,7 @@ def maze_solver(entry: tuple[int, int], exit: tuple[int, int], maze: list[list[i
                 checklist.append((y - 1, x))
             checked.append((y, x))
             checklist.pop(0)
+    if found_exit is False:
+        return "Path not found"
+    else:
+        return path_reconstruct(nodemaze, exit, entry)
